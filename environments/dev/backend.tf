cc5fc1bad3b005d07621779b999616a47326cb7c
Sandy124u <sandy.net89@gmail.com>
2025-10-25 21:54:18 +0530

    added backend dynamically 4

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "your-terraform-state-bucket"     # Replace with your actual bucket name
     key            = "eks/prod/terraform.tfstate"
-    region         = "ap-southeast-2"
+    region         = "us-east-1"
     use_lockfile   = true
     encrypt        = true
   }
