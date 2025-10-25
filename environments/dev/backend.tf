2f4c210adff28e75c67bbaafe693406a5a46ada2
Sandy124u <sandy.net89@gmail.com>
2025-10-25 22:53:28 +0530

    added backend dynamically 5

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"     # Replace with your actual bucket name
+    bucket         = "dynomo-db-bucket "     # Replace with your actual bucket name
     key            = "eks/prod/terraform.tfstate"
     region         = "us-east-1"
     use_lockfile   = true
