b8aab4c9da1b0da10afa48275605210624eabfe0
Sandy124u <sandy.net89@gmail.com>
2025-10-26 12:52:02 +0530

    added backend dynamically 6

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -7,3 +7,19 @@ module "eks" {
   node_count      = 2
   environment     = terraform.workspace
 }
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Environment = terraform.workspace
+    Name        = "terraform-locks"
+  }
+}
