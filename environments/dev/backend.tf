08ac52cb571f3575d5f9d5920c2cab6d811b88fc
Sandy124u <sandy.net89@gmail.com>
2025-10-27 13:26:08 +0530

    conflict region

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "dynomo-db-bucket"  # ✅ Remove trailing space
     key            = "eks/prod/terraform.tfstate"
-    region         = "region=ap-southeast-2"
+    region         = "ap-southeast-2"
     dynamodb_table = "terraform-locks"   # ✅ Enables state locking
     encrypt        = true
   }
