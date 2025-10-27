c2cc4f3ece14a1a1a9b8dba21a1205d443105737
Sandy124u <sandy.net89@gmail.com>
2025-10-27 13:10:49 +0530

    region change

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "dynomo-db-bucket"  # ✅ Remove trailing space
     key            = "eks/prod/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true   # ✅ Enables state locking
+    region         = "region=ap-southeast-2"
+    dynamodb_table = "terraform-locks"   # ✅ Enables state locking
     encrypt        = true
   }
 }
