6b20a421024e84a3ef64af9eb94747ad044696c9
Sandy124u <sandy.net89@gmail.com>
2025-10-28 10:51:41 +0530

    new changes version vpc

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -17,8 +17,8 @@ module "eks" {
   source          = "../../modules/eks"
   cluster_name    = "eks-${terraform.workspace}"
   cluster_version = "1.29"
-  subnet_ids      = ["subnet-abc", "subnet-def"]
-  vpc_id          = "vpc-xyz"
+  subnet_ids      = ["subnet-020390a81da7c36ee", "subnet-07262c17075d54334", "subnet-0b9c8c2db24d0db10"]
+  vpc_id          = "vpc-0955bf71c3993ea5a"
   node_count      = 2
   environment     = terraform.workspace
   region          = "ap-southeast-2"  # ✅ Add this line
